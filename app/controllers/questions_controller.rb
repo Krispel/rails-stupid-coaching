caa3113803bed90587f55f2a1a987158902da3e7
ofek krispel <ofekrisple@gmail.com>
2020-03-17 09:57:24 -0700

    done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,9 @@
 class QuestionsController < ApplicationController
   def ask
-
   end
 
   def answer
     @question = params[:question]
-    @answer = ''
     if @question == 'I am going to work'
       @answer = 'Great!'
       return @answer
